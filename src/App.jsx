f0458937939f41a00e01636b38ee21a1ac24c76e
Pablojonh6550 <Pablojonh6550@gmail.com>
2022-03-29 13:50:35 -0300

    version 1.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
-
+import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+// --CSS--
 import './App.css';
+
+// --Pages--
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
 
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <ul>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/newproject">New Project</Link>
+      </ul>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/company">
+          <Company />
+        </Route>
+        <Route exact path="/contact">
+          <Contact />
+        </Route>
+        <Route exact path="/newproject">
+          <NewProject /> 
+        </Route>
+      </Switch>
+      <p>Footer</p>
+    </Router>
   );
 }
 
